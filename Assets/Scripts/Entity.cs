49621fd3e53795ade76242b50200796308f5a062
Christian Pereda <christian-jesus.pereda-rives.1@ulaval.ca>
2024-07-29 18:57:48 -0400

    new repainted juana

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -11,6 +11,8 @@ public class Entity : MonoBehaviour
     #endregion
 
     [Header("Collision info")]
+    public Transform attackCheck;
+    public float attackCheckRadius;
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected float groundCheckDistance;
     [SerializeField] protected Transform wallCheck;
@@ -44,6 +46,7 @@ public class Entity : MonoBehaviour
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+        Gizmos.DrawWireSphere(attackCheck.position, attackCheckRadius);
     }
     #endregion
     #region Flip
